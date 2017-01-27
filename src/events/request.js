89414ce6c67d8afe051b3e0f7d75d420d7cd9d91
Geoff Dutton <g.dutton@gmail.com>
2017-01-26 23:52:02 -0600

    ready to publish

--- src/events/request.js
+++ src/events/request.js
@@ -145,20 +145,20 @@ class Request {
    * Returns true if the incoming request’s “Content-Type” HTTP header field matches the MIME type
    * specified by the type parameter. Returns false otherwise.
    *
-   // With Content-Type: text/html; charset=utf-8
-   req.is('html');
-   req.is('text/html');
-   req.is('text/*');
-   // => true
-
-   // When Content-Type is application/json
-   req.is('json');
-   req.is('application/json');
-   req.is('application/*');
-   // => true
-
-   req.is('html');
-   // => false
+   * // With Content-Type: text/html; charset=utf-8
+   * req.is('html');
+   * req.is('text/html');
+   * req.is('text/*');
+   * // => true
+   *
+   * // When Content-Type is application/json
+   * req.is('json');
+   * req.is('application/json');
+   * req.is('application/*');
+   * // => true
+   *
+   * req.is('html');
+   * // => false
    *
    * @param {string} type -- string or pattern
    * @returns {boolean}
